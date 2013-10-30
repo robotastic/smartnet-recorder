81353349dad1f74b9d4e49f3cd6a7087521f0802
Luke <lukekb@gmail.com>
2013-10-29 22:11:27 -0400

    working on op25

--- logging_receiver_p25.cc
+++ logging_receiver_p25.cc
@@ -43,11 +43,123 @@ log_p25::log_p25(float f, float c, long t)
 	freq = f;
 	center = c;
 	talkgroup = t;
+	double capture_rate = 5000000;
 	float offset = center - (f*1000000);
 	
+        float system_channel_rate = 125000;
+        float symbol_rate = 4800;
+        int symbol_deviation = 600.0;
+	double channel_decim = capture_rate / system_channel_rate
+        double channel_rate = capture_rate / channel_decim
+        double trans_width = 12500 / 2;
+        double trans_centre = trans_width + (trans_width / 2)
+
+
+        coeffs = gr.firdes.low_pass(1.0, capture_rate, trans_centre, trans_width, gr.firdes.WIN_HANN)
+        self.channel_filter = gr.freq_xlating_fir_filter_ccf(channel_decim, coeffs, 0.0, capture_rate)
+        self.set_channel_offset(0.0, 0, self.spectrum.win._units)
+        # power squelch
+        squelch_db = 0
+        self.squelch = gr.pwr_squelch_cc(squelch_db, 1e-3, 0, True)
+        self.set_squelch_threshold(squelch_db)
+        # FM demodulator
+        fm_demod_gain = channel_rate / (2.0 * pi * self.symbol_deviation)
+        fm_demod = gr.quadrature_demod_cf(fm_demod_gain)
+        # symbol filter        
+        symbol_decim = 1
+        # symbol_coeffs = gr.firdes.root_raised_cosine(1.0, channel_rate, self.symbol_rate, 0.2, 500)
+        # boxcar coefficients for "integrate and dump" filter
+        samples_per_symbol = channel_rate // self.symbol_rate
+        symbol_coeffs = (1.0/samples_per_symbol,)*samples_per_symbol
+        symbol_filter = gr.fir_filter_fff(symbol_decim, symbol_coeffs)
+        # C4FM demodulator
+        autotuneq = gr.msg_queue(2)
+        self.demod_watcher = demod_watcher(autotuneq, self.adjust_channel_offset)
+        demod_fsk4 = op25.fsk4_demod_ff(autotuneq, channel_rate, self.symbol_rate)
+        # symbol slicer
+        levels = [ -2.0, 0.0, 2.0, 4.0 ]
+        slicer = op25.fsk4_slicer_fb(levels)
+
+
+
+prefilter = gr_make_freq_xlating_fir_filter_ccf(channel_decim, 
+						       gr_firdes::low_pass(1.0, capture_rate, trans_centre, trans_width),
+						       offset, 
+						       capture_rate);
+
+
+	tune_queue = gr_make_msg_queue();
+	traffic_queue = gr_make_msg_queue();
+	const float l[] = { -2.0, 0.0, 2.0, 4.0 };
+	std::vector<float> levels( l,l + sizeof( l ) / sizeof( l[0] ) );
+	op25_demod = op25_make_fsk4_demod_ff(tune_queue, channel_rate, 4800);
+	op25_decoder = op25_make_decoder_bf();
+	op25_slicer = op25_make_fsk4_slicer_fb(levels);
+	op25_decoder->set_msgq(traffic_queue);
+
+	demod = gr_make_quadrature_demod_cf(channel_rate/(2.0 * pi * symbol_deviation));
+
+	unsigned int d = GCD(channel_rate, pre_channel_rate);
+    	channel_rate = floor(channel_rate  / d);
+    	pre_channel_rate = floor(pre_channel_rate / d);
+
+	downsample_sig = gr_make_rational_resampler_base_ccf(channel_rate, pre_channel_rate, design_filter(channel_rate, pre_channel_rate)); 
+
+
+	
+/*
+	d = GCD(audio_rate, vocoder_rate);
+    	audio_rate = floor(audio_rate  / d);
+    	vocoder_rate = floor(vocoder_rate / d);
+	upsample_audio = gr_make_rational_resampler_base_fff(audio_rate, vocoder_rate, design_filter(audio_rate, vocoder_rate));
+*/
+	
+	//demod = gr_make_quadrature_demod_cf(1.6);
+	
+
+	//const float a[] = { 0.1, 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1};
+	const float a[] = { 1.0/6, 1.0/6, 1.0/6, 1.0/6, 1.0/6, 1.0/6};
+
+
+   	std::vector<float> data( a,a + sizeof( a ) / sizeof( a[0] ) );
+	sym_filter = gr_make_fir_filter_fff(1, data); 
+
+
+	tm *ltm = localtime(&starttime);
+	
+	std::stringstream path_stream;
+	path_stream << boost::filesystem::current_path().string() <<  "/" << 1900 + ltm->tm_year << "/" << 1 + ltm->tm_mon << "/" << ltm->tm_mday;
+	
+	boost::filesystem::create_directories(path_stream.str());
+	//sprintf(filename, "%s/%ld-%ld.wav", path_stream.str().c_str(),talkgroup,timestamp);
+
+	sprintf(filename, "%ld-%ld.wav",talkgroup,timestamp);
+	
+	wav_sink = gr_make_wavfile_sink(filename,1,8000,16);
+
+	
+	//gr_file_sink_sptr fs = gr_make_file_sink(sizeof(float),"sym_filter.dat");
+
+
+
+	
+	connect(self(), 0, prefilter, 0);
+	
+	muted  = true;
+
+	connect(prefilter, 0, downsample_sig, 0);
+	connect(downsample_sig, 0, demod, 0);
+	connect(demod, 0, sym_filter, 0);
+	connect(sym_filter, 0, op25_demod, 0);
+	connect(op25_demod,0, op25_slicer, 0);
+	connect(op25_slicer,0, op25_decoder,0);
+	connect(op25_decoder, 0, wav_sink,0);
+
+
+
 	int samp_per_sym = 10;  //6
 	double samp_rate = 5000000;
-	int decim = 160;  //20
+	int decim = 80;  //20
 	float xlate_bandwidth = 14000; //24260.0;
 	float channel_rate = 4800 * samp_per_sym;
 	double pre_channel_rate = double(samp_rate/decim);
@@ -104,15 +216,6 @@ log_p25::log_p25(float f, float c, long t)
 
    	std::vector<float> data( a,a + sizeof( a ) / sizeof( a[0] ) );
 	sym_filter = gr_make_fir_filter_fff(1, data); 
-	//dsd = dsd_make_block_ff(dsd_FRAME_P25_PHASE_1,dsd_MOD_AUTO_SELECT,3,2,0, false, talkgroup);
-
-
-	//null_sink = gr_make_null_sink(sizeof(gr_complex));
-null_sink = gr_make_null_sink(sizeof(float));
-	null_source = gr_make_null_source(sizeof(gr_complex));
-	copier = gr_make_kludge_copy(sizeof(gr_complex));
-	head_source = gr_make_head(sizeof(gr_complex),0);
-	sprintf(filename, "%ld-%ld.wav", talkgroup,timestamp);
 
 
 	tm *ltm = localtime(&starttime);
@@ -135,12 +238,6 @@ null_sink = gr_make_null_sink(sizeof(float));
 	
 	connect(self(), 0, prefilter, 0);
 	
-
-	//start off muted	
-/*
-	connect(self(), 0, null_sink, 0);
-	connect(null_source,0,head_source,0);
-	connect(head_source,0,prefilter,0);*/
 	muted  = true;
 
 	connect(prefilter, 0, downsample_sig, 0);
