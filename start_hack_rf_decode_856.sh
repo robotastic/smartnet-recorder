a46627182f1f68accfa81886651fdff7f24b2134
robotastic <luke@robotastic.com>
2014-10-11 16:51:23 -0400

    no message

--- start_hack_rf_decode_856.sh
+++ start_hack_rf_decode_856.sh
@@ -2,4 +2,4 @@ ulimit -c unlimited
 
 #valgrind --tool=memcheck --leak-check=full ./smartnet --freq 856187500.0 --center 857600000.0  --rate 5000000 --error -5300 --rfgain 14 --ifgain 20 --bbgain 20
 
-./smartnet --freq 856187500.0 --center 858000000.0  --rate 8000000 --error 14300 --rfgain 14 --ifgain 25 --bbgain 25
+./smartnet --freq 856187500.0 --center 858000000.0  --rate 8000000 --error 13900 --rfgain 14 --ifgain 24 --bbgain 36
