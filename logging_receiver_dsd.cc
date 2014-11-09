030bcc8f3258020aab637300897c954ff18c6815
robotastic <luke@robotastic.com>
2014-11-09 14:30:04 -0500

    prefilter

--- logging_receiver_dsd.cc
+++ logging_receiver_dsd.cc
@@ -100,7 +100,7 @@ log_dsd::log_dsd(float f, float c, long s, long t, int n)
 	sprintf(raw_filename, "%s/%ld-%ld_%g.raw", path_stream.str().c_str(),talkgroup,timestamp,freq);
   	sprintf(status_filename, "%s/%ld-%ld_%g.json", path_stream.str().c_str(),talkgroup,timestamp,freq);
 	wav_sink = gr::blocks::wavfile_sink::make(filename,1,8000,16);
-	
+
 	raw_sink = gr::blocks::file_sink::make(sizeof(gr_complex), raw_filename);
 	null_sink = gr::blocks::null_sink::make(sizeof(gr_complex));
 
@@ -160,10 +160,9 @@ void log_dsd::deactivate() {
 
 	wav_sink->close();
 	
-	if (iam_logging) {
 		raw_sink->close();
-		disconnect(self(),0, raw_sink,0);
-	}
+		disconnect(prefilter,0, raw_sink,0);
+
 	
 	disconnect(self(), 0, prefilter, 0);
 	connect(self(),0, null_sink,0);
@@ -247,18 +246,16 @@ void log_dsd::activate(float f, int t, int n) {
 	sprintf(status_filename, "%s/%ld-%ld_%g.json", path_stream.str().c_str(),talkgroup,timestamp,freq);
     raw_sink->open(raw_filename);
 	
-	if (iam_logging) {
 		sprintf(raw_filename, "%s/%ld-%ld_%g.raw", path_stream.str().c_str(),talkgroup,timestamp,freq);
 		wav_sink->open(filename);
-	}
+
 	
 	lock();
 	disconnect(self(),0, null_sink, 0);
 	connect(self(),0, prefilter,0);
 	connect(prefilter, 0, downsample_sig, 0);
-	if (iam_logging) {
-		connect(self(),0, raw_sink,0);
-	}
+
+		connect(prefilter,0, raw_sink,0);
 	connect(downsample_sig, 0, demod, 0);
 	connect(demod, 0, sym_filter, 0);
 	connect(sym_filter, 0, levels, 0);
