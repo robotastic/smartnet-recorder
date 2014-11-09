4b2ebf8868b7dd35d9fcd8baf2d7fe4d213fd657
robotastic <luke@robotastic.com>
2014-11-08 20:32:49 -0500

    raw

--- logging_receiver_dsd.h
+++ logging_receiver_dsd.h
@@ -112,7 +112,7 @@ private:
 	gr::analog::quadrature_demod_cf::sptr demod;
 	dsd_block_ff_sptr dsd;
 	gr::blocks::wavfile_sink::sptr wav_sink;
-	gr::blocks::file_sink::sptr file_sink;
+	gr::blocks::file_sink::sptr raw_sink;
 	gr::blocks::null_sink::sptr null_sink;
 	gr::blocks::head::sptr head_source;
 	//gr_kludge_copy_sptr copier;
