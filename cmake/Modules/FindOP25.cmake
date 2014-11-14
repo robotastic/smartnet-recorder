93528e6d044de776665b553ea968829e602825ea
robotastic <luke@robotastic.com>
2014-11-13 21:43:27 -0500

    this?

--- cmake/Modules/FindOP25.cmake
+++ cmake/Modules/FindOP25.cmake
@@ -1,21 +1,29 @@
-if(NOT OP25_FOUND)
-  pkg_check_modules (OP25_PKG op25)
-
-  find_library(OP25_LIBRARIES NAMES gr-dsd
-    PATHS
-    ${LIBDSD_PKG_LIBRARY_DIRS}
-    /usr/lib
-    /usr/local/lib
+INCLUDE(FindPkgConfig)
+if(NOT LIBOP25_FOUND)
+  pkg_check_modules (LIBOP25_PKG libop25)
+  find_path(LIBOP25_INCLUDE_DIR
+    NAMES op25/api.h
+    HINTS ${LIBOP25_PKG_INCLUDE_DIRS}
+    PATHS /usr/include
+          /usr/local/include
   )
 
-if(LIBDSD_INCLUDE_DIR AND LIBDSD_LIBRARIES)
-  set(LIBDSD_FOUND TRUE CACHE INTERNAL "libdsd found")
-  message(STATUS "Found libdsd: ${LIBDSD_INCLUDE_DIR}, ${LIBDSD_LIBRARIES}")
-else(LIB_INCLUDE_DIR AND LIBDSD_LIBRARIES)
-  set(LIBDSD_FOUND FALSE CACHE INTERNAL "libdsd found")
-  message(STATUS "libdsd not found.")
-endif(LIBDSD_INCLUDE_DIR AND LIBDSD_LIBRARIES)
+  find_library(LIBOP25_LIBRARIES
+    NAMES gr-op25
+    HINTS ${LIBOP25_PKG_LIBRARY_DIRS}
+    PATHS /usr/lib
+          /usr/local/lib
+  )
+message(STATUS "Pkg: ${LIBOP25_PKG}, ${LIBOP25_PKG_INCLUDE_DIRS}, ${LIBOP25_PKG_LIBRARY_DIRS}")
+message(STATUS "Vars: ${LIBOP25_INCLUDE_DIR}, ${LIBOP25_LIBRARIES}")
+if(LIBOP25_INCLUDE_DIR AND LIBOP25_LIBRARIES)
+  set(LIBOP25_FOUND TRUE CACHE INTERNAL "libop25 found")
+  message(STATUS "Found libop25: ${LIBOP25_INCLUDE_DIR}, ${LIBOP25_LIBRARIES}")
+else(LIBOP25_INCLUDE_DIR AND LIBOP25_LIBRARIES)
+  set(LIBOP25_FOUND FALSE CACHE INTERNAL "libop25 found")
+  message(STATUS "libop25 not found.")
+endif(LIBOP25_INCLUDE_DIR AND LIBOP25_LIBRARIES)
 
-mark_as_advanced(LIBDSD_INCLUDE_DIR LIBDSD_LIBRARIES)
+mark_as_advanced(LIBOP25_INCLUDE_DIR LIBOP25_LIBRARIES)
 
-endif(NOT LIBDSD_FOUND)
+endif(NOT LIBOP25_FOUND)
