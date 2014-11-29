7ad1a89aeda4c9a7884366b55b5848ed0fd460d4
robotastic <luke@robotastic.com>
2014-11-29 14:59:28 -0500

    f

--- logging_receiver_p25.cc
+++ logging_receiver_p25.cc
@@ -113,16 +113,16 @@ std::cout << "After GCD - Prechannel Decim: " << prechannel_decim << " Rate: " <
 	int udp_port = 0;
 	int verbosity = 10;
 	const char * wireshark_host="127.0.0.1";
-	bool do_imbe = 0;
+	bool do_imbe = 1;
 	bool do_output = 1;
 	bool do_msgq = 0;
-	bool do_audio_output = 0;
+	bool do_audio_output = 1;
 	bool do_tdma = 0;
 	op25_frame_assembler = gr::op25_repeater::p25_frame_assembler::make(wireshark_host,udp_port,verbosity,do_imbe, do_output, do_msgq, rx_queue, do_audio_output, do_tdma);
 	//op25_vocoder = gr::op25_repeater::vocoder::make(0, 0, 0, "", 0, 0);
 
-    //converter = gr::blocks::short_to_float::make();
-    converter = gr::blocks::char_to_float::make();
+    converter = gr::blocks::short_to_float::make();
+    //converter = gr::blocks::char_to_float::make();
     float convert_num = float(1.0)/float(32768.0);
     multiplier = gr::blocks::multiply_const_ff::make(convert_num);
 	tm *ltm = localtime(&starttime);
@@ -225,12 +225,12 @@ void log_p25::deactivate() {
 		
 
 	disconnect(op25_demod,0, op25_slicer, 0);
-	disconnect(multiplier, 0, dump_sink, 0);
+	//disconnect(multiplier, 0, dump_sink, 0);
 	
 	disconnect(op25_slicer,0, op25_frame_assembler,0);
 	disconnect(op25_frame_assembler, 0,  converter,0);
     disconnect(converter, 0, multiplier,0);
-    /*disconnect(multiplier, 0, wav_sink,0);*/
+    disconnect(multiplier, 0, wav_sink,0);
 	
 
 	unlock();
@@ -288,13 +288,13 @@ void log_p25::activate(float f, int t, int n) {
 	
 	connect(op25_demod,0, op25_slicer, 0);
 	connect(sym_filter, 0, op25_demod, 0);
-	connect(multiplier, 0, dump_sink, 0);
+	//connect(multiplier, 0, dump_sink, 0);
 
 
 	connect(op25_slicer,0, op25_frame_assembler,0);
 	connect(op25_frame_assembler, 0,  converter,0);
     connect(converter, 0, multiplier,0);
-    /*connect(multiplier, 0, wav_sink,0);*/
+    connect(multiplier, 0, wav_sink,0);
 	
 	unlock();
 	active = true;
