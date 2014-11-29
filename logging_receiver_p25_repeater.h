306b541956bd8937deaa2cec4bd7216a3407fa73
robotastic <luke@robotastic.com>
2014-11-29 08:13:43 -0500

    f

--- logging_receiver_p25_repeater.h
+++ logging_receiver_p25_repeater.h
@@ -45,7 +45,7 @@
 #include <op25/fsk4_demod_ff.h>
 #include <op25/fsk4_slicer_fb.h>
 #include <op25_repeater/fsk4_slicer_fb.h>
-#include <op25_repeater/op25_frame_assembler.h>
+#include <op25_repeater/p25_frame_assembler.h>
 #include <op25_repeater/gardner_costas_cc.h>
 #include <op25_repeater/vocoder.h>
 
