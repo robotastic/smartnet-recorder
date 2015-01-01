56f4ca0ae9e914463c4b3fd987f5a93e07e1e040
Compugician <tal@compugician.com>
2015-01-01 16:38:01 -0500

    encoder script, upload disabled

---
+++ encode-upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Encoding: $1"
+filename="$1"
+basename="${filename%.*}"
+#mp3encoded="$basename.mp3"
+mp4encoded="$basename.m4a"
+json="$basename.json"
+#eval "lame --preset voice" ${filename}
+#eval "/home/luke/bin/ffmpeg -i $filename  -c:a libfdk_aac -vbr 3 $mp4encoded > /dev/null 2>&1"
+eval "avconv -i $filename -c:a alac $mp4encoded >> eval_avconv.log 2>&1"
+
+#echo "Upload: $encoded"
+#eval "scp $json $mp4encoded username@website.com:~/smartnet-upload "
+
